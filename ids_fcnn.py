01d7da3e43b39cb717c00c05792e5e258b9d99e8
vintagedeek <derekgriffing@gmail.com>
2020-07-22 14:28:34 -0400

    Update ids_fcnn.py
    Some thoughts from DG commented in code, with some proposed changes (e.g., handling relu fxn in forward method only).

--- ids_fcnn.py
+++ ids_fcnn.py
@@ -18,14 +18,16 @@ class SimpleNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear1 = nn.Linear(ids_features, ids_features)
-        self.relu1 = F.relu
+        # self.relu1 = F.relu I believe this should be removed as the ReLU activation for layer 'linear1' is applied in the forward method.
+        # Note this would be handled differently if you were using nn.Sequential to build the architecture. 
         self.linear2 = nn.Linear(ids_features, ids_classes)
-    
+   
     # Perform the computation
     def forward(self, x):
         x = self.linear1(x)
-        x = self.relu1(x)
-        x = self.linear2(x)
+        x = F.relu(x) # I believe you want to use F.relu(x) here.
+        x = self.linear2(x) # Not sure, but I think a non-linearity activation function (e.g., x = F.sigmoid(x)) would be helpful. 
+        # note, combining a sigmoid activation fxn in outer layer with cross entropy cost function helps to reduce neuron saturation/learning slowdown. 
         return x
 
 # Define a utility function to train the model
@@ -67,7 +69,9 @@ learning_rate = 0.001
 opt = torch.optim.SGD(model.parameters(), learning_rate)
 
 # Define loss function as mean squared error
-loss_fn = F.mse_loss
+loss_fn = F.mse_loss # I think cross_entropy may be better for classification. Per above, cross entropy works better with a sigmoid outer 
+# layer activation (if you choose to try it) compared to a sigmoid outer layer with a MSE/quadratic cost function. 
+# I believe F.cross_entropy(predictions, labels) prefers labels to be integers (e.g., 0,1,2,3,4,5,6,7,8,9 for MNIST).
 
 # Record initial value of loss
 loss = loss_fn(model(train_inputs), train_targets)
